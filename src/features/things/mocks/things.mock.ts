63c84dc20e4f2c8e488090db634bddd22db61ae0
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-26 19:14:19 +0100

    Add reducer test

---
+++ src/features/things/mocks/things.mock.ts
@@ -0,0 +1,22 @@
+export const MOCK_THINGS = [
+  {
+    id: 1,
+    name: "Cosa 1",
+    howInteresting: 1,
+    howImportant: 1,
+  },
+  {
+    id: 2,
+    name: "Cosa 2",
+    howInteresting: 1,
+    howImportant: 1,
+  },
+  {
+    id: 3,
+    name: "Cosa 3",
+    howInteresting: 1,
+    howImportant: 1,
+  },
+  { id: 4, name: "Cosa 4", howInteresting: 8, howImportant: 9 },
+  { id: 5, name: "Cosa 5", howInteresting: 9, howImportant: 1 },
+];
---
+++ src/features/things/reducer/thing.reducer.test.ts
@@ -0,0 +1,61 @@
+import { ThingStructure } from "../models/things";
+import { thingReducer } from "./thing.reducer";
+import {
+  addCreator,
+  deleteCreator,
+  load1Creator,
+  loadCreator,
+  updateCreator,
+} from "./things.action.creator";
+
+describe("Given the thingReducer function", () => {
+  describe("When we load an array of things", () => {
+    test("Then it should load an array of things", () => {
+      let result = thingReducer([], loadCreator);
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe("When we load a single object from the array of things", () => {
+    test("Then it should load a single object from MOCK_THINGS", () => {
+      let result = thingReducer([], load1Creator);
+      expect(result).toEqual([]);
+    });
+  });
+  describe("When we try to add an object", () => {
+    test("Then it should load a new item", () => {
+      const mock1: ThingStructure = {
+        id: 1,
+        name: "Cosa 1",
+        howInteresting: 1,
+        howImportant: 1,
+      };
+      let result = thingReducer([], addCreator(mock1));
+      expect(result).toEqual([mock1]);
+    });
+  });
+  describe("When we try to delete an object", () => {
+    test("Then it should delete a new item", () => {
+      let result = thingReducer([], deleteCreator);
+      expect(result).toEqual([]);
+    });
+  });
+  describe("When we try to update (edit) an object", () => {
+    test("Then it should update a new item", () => {
+      const mock2: ThingStructure = {
+        id: 1,
+        name: "Cosa 1",
+        howInteresting: 1,
+        howImportant: 1,
+      };
+      const mock3: ThingStructure = {
+        id: 1,
+        name: "Cosa 2",
+        howInteresting: 1,
+        howImportant: 1,
+      };
+      let result = thingReducer([mock2], updateCreator(mock3));
+      expect(result).toEqual([mock3]);
+    });
+  });
+});
